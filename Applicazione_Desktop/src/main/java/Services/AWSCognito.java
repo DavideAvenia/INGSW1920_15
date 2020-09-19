f925479d41e5198f840e561fefca7fe7eb81fde1
Giuseppe Porcaro <porcarogiuseppe97@gmail.com>
2020-09-19 04:20:58 +0200

    Aggiustati diagrammi inerenti ad un bug trovato durante il testing
    Diagrammi cancella Utente e modifica utente. (Caso d'uso gestione accounts)

--- Applicazione_Desktop/src/main/java/Services/AWSCognito.java
+++ Applicazione_Desktop/src/main/java/Services/AWSCognito.java
@@ -19,9 +19,9 @@ import java.util.*;
 
 public class AWSCognito implements UtenteDao {
 
-    private final String USERPOOLID = "";
-    private final String CLIENTID = "";
-    private final String SECRET = "";
+    private final String USERPOOLID = "eu-west-1_KWhWZTu1x";
+    private final String CLIENTID = "66eho5mi1f4ift40cjtmvo03id";
+    private final String SECRET = "1e002tkp4rqratmgrlht6jvecsip96836j2e49tbph7j3lqfgi7s";
     private AWSCognitoIdentityProvider identityProvider;
 
     public AWSCognito() {
