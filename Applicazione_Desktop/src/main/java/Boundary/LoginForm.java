b52e3229d959de30e2383f4bc835bd0c386dfc93
Davide Avenia <54034699+DavideAvenia@users.noreply.github.com>
2020-09-23 13:36:11 +0200

    fix

--- Applicazione_Desktop/src/main/java/Boundary/LoginForm.java
+++ Applicazione_Desktop/src/main/java/Boundary/LoginForm.java
@@ -5,12 +5,18 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class LoginForm extends Application {
 
@@ -24,10 +30,12 @@ public class LoginForm extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/LoginForm.fxml"));
+        Scene scene = new Scene(root, 765.0,500.0);
         Image iconaStage = new Image("https://progettoingswfedericoii.s3-eu-west-1.amazonaws.com/iconaAppDesktop.png");
         stage.getIcons().add(iconaStage);
         stage.setTitle("Login");
-        stage.setScene(new Scene(root, 765.0,500.0));
+
+        stage.setScene(scene);
         stage.show();
     }
 
